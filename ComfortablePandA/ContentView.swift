c7cee3adbe254ace53f01ef78d041d2faabdb17c
das08 <takeda5955@gmail.com>
2020-10-05 00:26:22 +0900

    API fetch関連の実装テスト

--- ComfortablePandA/ContentView.swift
+++ ComfortablePandA/ContentView.swift
@@ -7,33 +7,46 @@
 
 import SwiftUI
 
+
 struct ContentView: View {
     
-    @AppStorage("kadai", store: UserDefaults(suiteName: "group.com.das08.ComfortablePandA"))
-    var kadaiList: Data = Data()
+//    @AppStorage("kadai", store: UserDefaults(suiteName: "group.com.das08.ComfortablePandA"))
+//    var kadaiList: Data = Data()
+//
+//    let kadais = [
+//        Kadai(id: "001", lectureName: "Lec1", assignmentInfo: "Quiz1", dueDate: generateDate(y: 2020, mo: 10, d: 4, h: 9, min: 0), isFinished: false),
+//        Kadai(id: "002", lectureName: "Lec2", assignmentInfo: "Quiz2", dueDate: generateDate(y: 2020, mo: 10, d: 8, h: 9, min: 0), isFinished: false),
+//        Kadai(id: "003", lectureName: "Lec3", assignmentInfo: "Quiz3", dueDate: generateDate(y: 2020, mo: 10, d: 8, h: 9, min: 0), isFinished: false)
+//    ]
+
     
-    let kadais = [
-        Kadai(id: "001", lectureName: "Lec1", assignmentInfo: "Quiz1", dueDate: generateDate(y: 2020, mo: 10, d: 4, h: 9, min: 0), isFinished: false),
-        Kadai(id: "002", lectureName: "Lec2", assignmentInfo: "Quiz2", dueDate: generateDate(y: 2020, mo: 10, d: 8, h: 9, min: 0), isFinished: false),
-        Kadai(id: "003", lectureName: "Lec3", assignmentInfo: "Quiz3", dueDate: generateDate(y: 2020, mo: 10, d: 8, h: 9, min: 0), isFinished: false)
-    ]
+//    var kadaiList: [AssignmentEntry] = loadKadai()!.assignment_collection
+    
     
     var body: some View {
+        
+
+        Text("1")
+            .onAppear(
+                perform: {
+                    let kadaiList = SakaiAPI.shared.getKK()
+                    print(kadaiList)
+                    
+                }
+            )
         VStack(spacing: 30){
-            ForEach(kadais) { kadai in
-                KadaiView(kadai: kadai)
-                    .onTapGesture {
-                        save(kadai)
-                    }
+            let kadaiList = SakaiAPI.shared.getKK()
+            ForEach(kadaiList){kadai in
+                Text(kadai.title)
             }
         }
     }
     
-    func save(_ kadai: Kadai){
-        guard let kadaiList = try? JSONEncoder().encode(kadai) else { return }
-        self.kadaiList = kadaiList
-        print("Save")
-    }
+//    func save(_ kadai: Kadai){
+//        guard let kadaiList = try? JSONEncoder().encode(kadai) else { return }
+//        self.kadaiList = kadaiList
+//        print("Save")
+//    }
 }
 
 struct ContentView_Previews: PreviewProvider {
---
+++ ComfortablePandA/SakaiAssignment.swift
@@ -0,0 +1,115 @@
+//
+//  SakaiAssignment.swift
+//  ComfortablePandA
+//
+//  Created by das08 on 2020/10/04.
+//
+import Foundation
+
+final class SakaiAPI {
+    
+    static let shared = SakaiAPI()
+    
+    func fetchAssignmentsFromPandA(completion: @escaping ([AssignmentEntry]) -> ()) {
+        
+        let urlString = "https://das82.com/my.json"
+        let url = URL(string: urlString)!
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print("data is nil")
+                return
+            }
+            
+            guard let kadaiList = try? JSONDecoder().decode(SakaiAssignment.self, from: data) else {
+                print("cannnot get kadai")
+                return
+            }
+            
+            completion(kadaiList.assignment_collection)
+//            print(kadaiList.assignment_collection)
+//            print("success")
+        }
+        task.resume()
+    }
+    
+    enum SakaiError: Error{
+        case url
+    }
+    
+    func fetchAssignmentsFromPandA2() throws -> [AssignmentEntry]? {
+        
+        let urlString = "https://das82.com/my.json"
+        let url = URL(string: urlString)!
+        
+        var assignmentEntry: [AssignmentEntry]?
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                print("data is nil")
+                return
+            }
+            
+            guard let kadaiList = try? JSONDecoder().decode(SakaiAssignment.self, from: data) else {
+                print("cannnot get kadai")
+                return
+            }
+            
+            assignmentEntry = kadaiList.assignment_collection
+            semaphore.signal()
+        }
+        task.resume()
+        
+        _ = semaphore.wait(timeout: .distantFuture)
+        
+        return assignmentEntry
+    }
+    
+    func getKK() -> [AssignmentEntry] {
+        var kadaiList = [AssignmentEntry]()
+        let anonymousFunc = { (fetchedKadaiList: [AssignmentEntry]) in
+            
+            kadaiList = fetchedKadaiList
+//            print(kadaiList)
+            
+            
+        }
+        print(kadaiList)
+        DispatchQueue.main.async {
+            SakaiAPI.shared.fetchAssignmentsFromPandA(completion: anonymousFunc)
+        }
+        
+        return kadaiList
+    }
+}
+
+struct SakaiAssignment: Codable {
+    let assignment_collection: [AssignmentEntry]
+    init() {
+        assignment_collection = [AssignmentEntry()]
+    }
+}
+
+struct AssignmentEntry: Codable, Identifiable {
+    let context: String
+    let id: String
+    let title: String
+    let dueTime: AssignmentEntryDueTime
+    init() {
+        context = ""
+        id = ""
+        title = ""
+        dueTime = AssignmentEntryDueTime()
+    }
+}
+
+struct AssignmentEntryDueTime: Codable {
+    let time: Int
+    init() {
+        time = 0
+    }
+}
+
+
+
